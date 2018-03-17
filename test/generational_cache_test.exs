aeb9216aa800f6438144344e6bf185178ce1d17f
Josh Bodah <joshuabodah@gmail.com>
2018-03-17 10:51:49 -0400

    add tests

--- test/generational_cache_test.exs
+++ test/generational_cache_test.exs
@@ -2,7 +2,37 @@ defmodule GenerationalCacheTest do
   use ExUnit.Case
   doctest GenerationalCache
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "empty cache has no entries" do
+    cache = GenerationalCache.new(3)
+    assert %{} == GenerationalCache.entries(cache)
+  end
+
+  test "returns all entries" do
+    cache = GenerationalCache.new(3)
+    cache = GenerationalCache.add_generation(cache, %{a: 3})
+    cache = GenerationalCache.add_generation(cache, %{b: 4})
+    assert %{a: 3, b: 4} == GenerationalCache.entries(cache)
+  end
+
+  test "last write wins" do
+    cache = GenerationalCache.new(3)
+    cache = GenerationalCache.add_generation(cache, %{a: 3})
+    cache = GenerationalCache.add_generation(cache, %{a: 4})
+    assert %{a: 4} == GenerationalCache.entries(cache)
+  end
+
+  test "partial overlaps" do
+    cache = GenerationalCache.new(3)
+    cache = GenerationalCache.add_generation(cache, %{a: 3, b: 4})
+    cache = GenerationalCache.add_generation(cache, %{b: 5, c: 6})
+    assert %{a: 3, b: 5, c: 6} == GenerationalCache.entries(cache)
+  end
+
+  test "old generations are purged" do
+    cache = GenerationalCache.new(2)
+    cache = GenerationalCache.add_generation(cache, %{a: 3})
+    cache = GenerationalCache.add_generation(cache, %{b: 4})
+    cache = GenerationalCache.add_generation(cache, %{c: 5})
+    assert %{b: 4, c: 5} == GenerationalCache.entries(cache)
   end
 end
